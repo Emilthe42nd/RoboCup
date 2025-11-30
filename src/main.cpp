b6dcfb5bac239efd28ca784ba315ee63569d59a2
Emilthe42nd <emil42424242@gmail.com>
2025-11-30 22:21:18 +0100

    added Helper and PIO Infrastructure

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <utils.cpp>
+
+using namespace Robot;
+
+void setup() {
+
+}
+
+
+
+void loop() {
+  // main loop goes here
+}
+
---
+++ src/utils.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+
+
+using namespace Robot;
+
+#define PI 3.14159
+
+namespace Robot {
+  namespace Helper {
+
+    float convertToDegrees(float& angleRad) {
+      return angleRad * (180.0f / PI);
+    }
+
+    float convertToRad(float& angleDegrees) {
+      return angleDegrees * (PI / 180.0f);
+    }
+
+    float getMagnitude2D(float Vector[2]) {
+
+      // Maybe remove sqareSum declaration if PROGMEM is tight.
+      // NOTE: yes, this is the easiest way to sqare a number in c++
+      // TODO: If it is more readable, maybe add a helper::sqare() function
+
+      float sqareSum = Vector[0] * Vector[0] + Vector[1] * Vector[1];
+      return sqrt(sqareSum);
+    }
+
+    float getMagnitude3D(float Vector[3]) {
+      float sqareSum = Vector[0] * Vector[0] + Vector[1] * Vector[1] + Vector[2] * Vector[2];
+      return sqrt(sqareSum);
+    }
+
+    float scalarProduct(float Vector1[2], float Vector2[2]) {
+      return Vector1[0] * Vector2[0] + Vector1[1] * Vector2[1];
+    }
+
+    float getVectorAngle(float Vector1[2], float Vector2[2]) {
+      float angleDeg = acos(scalarProduct(Vector1, Vector2) / (getMagnitude2D(Vector1) * getMagnitude2D(Vector2)));
+      return angleDeg;
+    }
+  }
+}
